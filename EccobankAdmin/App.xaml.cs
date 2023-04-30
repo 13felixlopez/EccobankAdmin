d79960c79daae01c120a54834da0f57d83abed16
Felix Lopez <lfelixfermin@gmail.com>
2023-04-30 13:45:34 -0600

    Agregar archivos de proyecto.

---
+++ EccobankAdmin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using EccobankAdmin.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EccobankAdmin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Menuprincipal();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
