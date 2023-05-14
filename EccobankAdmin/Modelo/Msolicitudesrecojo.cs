df39ab014021dc079d030422149ea315c54e83c5
Felix Lopez <lfelixfermin@gmail.com>
2023-05-14 09:29:08 -0600

    ADD:Dsolicitudesrecojo,Msolicitudesrecojo,Asignaciones UPDATE:VMmenuprincipal,Menuprincipal

---
+++ EccobankAdmin/Modelo/Msolicitudesrecojo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EccobankAdmin.Modelo
+{
+    public class Msolicitudesrecojo
+    {
+        public string Estado { get; set; }
+        public string Fecha { get; set; }
+        public string Idcliente { get; set; }
+        public string Idturno { get; set; }
+        public string Idsolicitud { get; set; }
+    }
+}
--- EccobankAdmin/VistaModelo/VMmenuprincipal.cs
+++ EccobankAdmin/VistaModelo/VMmenuprincipal.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using EccobankAdmin.Vistas.Config;
 using EccobankAdmin.Vistas;
+using EccobankAdmin.Datos;
+using EccobankAdmin.Modelo;
 
 namespace EccobankAdmin.VistaModelo
 {
@@ -12,24 +14,24 @@ namespace EccobankAdmin.VistaModelo
     {
         #region VARIABLES
         string identificacion;
-        //List<Msolicitudesrecojo> listasolRecojo;
+        List<Msolicitudesrecojo> listasolRecojo;
         #endregion
         #region CONSTRUCTOR
         public VMmenuprincipal(INavigation navigation)
         {
             Navigation = navigation;
             Navegarmenuconfigcomamd = new Command(async () => await NavegarMenuconfig());
-            //NavegarAsignacionescomamd = new Command<Msolicitudesrecojo>(async (f) => await NavegarAsignaciones(f));
+            Navegarasignacionesgcomamd = new Command<Msolicitudesrecojo>(async (f) => await NavegarAsignaciones(f));
 
-           // Mostrarsolicitudesrecojo();
+           Mostrarsolicitudesrecojo();
         }
         #endregion
         #region OBJETOS 
-       /* public List<Msolicitudesrecojo> ListasolRecojo
+        public List<Msolicitudesrecojo> ListasolRecojo
         {
             get { return listasolRecojo; }
             set { SetValue(ref listasolRecojo, value); }
-        }*/
+        }
         public string Identificacion
         {
             get { return identificacion; }
@@ -42,9 +44,22 @@ namespace EccobankAdmin.VistaModelo
         {
             await Navigation.PushAsync(new Menuconfig());
         }
+        private async Task NavegarAsignaciones(Msolicitudesrecojo parametros)
+        {
+            string Idsolicitud = parametros.Idsolicitud;
+
+            await Navigation.PushAsync(new Asignaciones());
+        }
+        private async Task Mostrarsolicitudesrecojo()
+        {
+            var funcion = new Dsolicitudesrecojo();
+            ListasolRecojo = await funcion.Mostrarsolicitudesrecojo();
+        }
         #endregion
         #region Comandos
         public Command Navegarmenuconfigcomamd { get; }
+        public Command Navegarasignacionesgcomamd { get; }
+
         #endregion
     }
 }
